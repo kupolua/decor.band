b87b359493bca3597f6138bcf107e7d0f724abdf
pavlokulakovskiy <pkulakovsky@gmail.com>
2019-05-18 01:40:25 +0300

    deploy!

--- docker/scripts/generate-site.sh
+++ docker/scripts/generate-site.sh
@@ -6,7 +6,7 @@ folderName=`echo ${GIT_URL} | sed -n 's/.*\/\([^ ]*\).git/\1/p'`
 
 cd $folderName
 
-bin/linux_x86_64/site_builder -generate -folder .
+bin/linux_arm/site_builder -generate -folder .
 
 git config --global user.email ${GIT_EMAIL}
 git config --global user.name ${GIT_NAME}
